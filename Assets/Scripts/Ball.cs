295b276c493dbbf121107660f55e253797a04d85
ryan4gitee <rliang.lai@outlook.com>
2021-10-14 21:32:14 +0800

    Unity 2d sample.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    bool m_isHit = false;
+
+    Vector3 m_startPos;
+
+    SpriteRenderer m_spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    bool IsHit()
+    {
+        m_isHit = false;
+
+        Vector3 ms = Input.mousePosition;
+
+        ms = Camera.main.ScreenToWorldPoint(ms);
+
+        Vector3 pos = this.transform.position;
+
+        float w = m_spriteRenderer.bounds.extents.x;
+        float h = m_spriteRenderer.bounds.extents.y;
+
+        if (ms.x > pos.x - w && ms.x < pos.x + w &&
+            ms.y > pos.y - h && ms.y < pos.y + h)
+        {
+            m_isHit = true;
+        }
+
+        return m_isHit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && IsHit())
+        {
+            m_startPos = Input.mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0) && m_isHit)
+        {
+            Vector3 endPos = Input.mousePosition;
+
+            Vector3 v = (m_startPos - endPos) * 3.0f;
+
+            this.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+
+            this.GetComponent<Rigidbody2D>().AddForce(v);
+        }
+    }
+}
---
+++ Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    protected SpriteRenderer m_sprite;
+
+    public Sprite[] m_clips;
+
+    protected float timer = 0.1f;
+
+    protected int m_frame = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_sprite = this.gameObject.GetComponent<SpriteRenderer>();
+        m_sprite.sprite = m_clips[m_frame];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            timer = 0.1f;
+
+            m_frame++;
+
+            if (m_frame >= m_clips.Length)
+            {
+                m_frame = 0;
+            }
+
+            m_sprite.sprite = m_clips[m_frame];
+        }
+    }
+}
